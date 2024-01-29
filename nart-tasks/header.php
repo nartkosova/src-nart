84536cab204b640bc8360677b34e13a72ef8d502
nartkosova <nartkosova21@gmail.com>
2024-01-29 10:51:05 +0200

    Fixed some issues

--- nart-tasks/header.php
+++ nart-tasks/header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title><?php echo $title;?></title>
-    
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header>
@@ -44,4 +44,3 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </header>
-    <link rel="stylesheet" href="styles.css">
