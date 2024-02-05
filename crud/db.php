6a51d396c1d4d062f599fbbde78f70f82777da4c
nartkosova <nartkosova21@gmail.com>
2024-02-05 11:42:44 +0200

    Exercise 7 CRUD app.

---
+++ crud/db.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "nart23000";
+$password ="QYPFY1uV";
+$dbname ="wp_nart23000";
+//create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
---
+++ crud/process.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_POST['submit'])){
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $city = $_POST['city'];
+    $groupid = $_POST['groupid'];
+// connect to database server
+include 'db.php';
+//write sql statements to insert data
+$sql = "insert into studentsinfo(first_name, last_name, city, groupid)
+        values ('$fname', '$lname', '$city', '$groupid')";
+if ($conn->query($sql)===TRUE){
+    echo "Your data was recorded";
+}else {
+    echo"Error: " .$sql . "<br>" . $conn->error;
+}
+//close connection
+$conn->close();
+}
+
+?>
